0318153bec3162df0f7e1cc0c5123e5d0bdd765d
ccczuo <229569731@qq.com>
2018-09-05 02:22:50 +0000

    layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
     <title>@yield('title','Webo')</title>
+    <link rel="stylesheet" href="/css/app.css">
+
 </head>
 <body>
+@include('layouts._header')
+
+<div class="container">
 @yield('content')
+    @include('layouts._footer')
+
+</div>
+
 </body>
 </html>
